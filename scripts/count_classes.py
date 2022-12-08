65fa7b4737b1ae94ae519dccda81fa2d030a902e
David Johnson <djohnson@techfak.uni-bielefeld.de>
2022-12-08 13:42:45 +0100

    Fixes counting script

--- scripts/count_classes.py
+++ scripts/count_classes.py
@@ -4,16 +4,22 @@ from src.data.affectnet_datasets import AffectNetImageDataset
 
 
 def calc_classweights(ds):
-    counts = ds.df['expression'].value_counts(normalize=True)
+    counts = ds.df['expression'].value_counts(normalize=False, sort=False)
     return counts
 
 def main():
     dataroot = Path('data/train_set')
-    ds = AffectNetImageDataset(dataroot, 'expression', name='train_data',
+    ds = AffectNetImageDataset(dataroot, 'expression', name='affectnet_img_train',
                                load_cache=True, save=False, keep_as_pandas=True)
     counts = calc_classweights(ds)
-    print(counts)
+    pct_list = []
+    for idx, val in counts.sort_index().iteritems():
+        pct_list.append(val)
+        print(f'{ds.expression_labels[int(idx)]}: {val}')
+
+    print(pct_list)
+
+
 
 if __name__ == '__main__':
     main()
-    
